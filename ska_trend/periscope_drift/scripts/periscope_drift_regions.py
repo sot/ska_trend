bfc57702b9e8076d459ead0ebb5017590241d4dd
Javier Gonzalez <javierggt@yahoo.com>
2025-11-17 09:56:22 -0500

    add ska_trend_periscope_drift_regions script

---
+++ ska_trend/periscope_drift/scripts/periscope_drift_regions.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+from astromon.scripts import excluded_region
+
+SKA = Path(os.environ["SKA"])
+
+
+def modified_parser(parser_function):
+    def get_parser():
+        parser = parser_function()
+        parser.description = "Periscope drift excluded regions management tool"
+        # the default is db_file=None, which causes $SKA/data/astromon/astromon.h5 to be used
+        parser.set_defaults(
+            db_file=SKA / "data" / "periscope_drift_reports" / "excluded_sources.h5"
+        )
+        return parser
+    return get_parser
+
+
+def main(args=None):
+    os.environ["ASTROMON_FILE"] = str(
+        SKA / "data" / "periscope_drift_reports" / "excluded_sources.h5"
+    )
+    for action, parser in excluded_region.PARSERS.items():
+        excluded_region.PARSERS[action] = modified_parser(parser)
+
+    excluded_region.main(args)
+
+
+if __name__ == "__main__":
+    main()
